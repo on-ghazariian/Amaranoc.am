c4e656c76209b72769b5e90fab528702e03c5563
on-ghazariian <ghazarian009ait@gmail.com>
2026-06-06 16:19:16 +0400

    create likes page

---
+++ amaranoc/src/pages/LIke.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../components/header/header'
+import Likes from '../components/header/search/likes'
+import FooterForm from '../components/footer/footer'
+
+export default function LIke() {
+  return (
+    <>
+       <Header/>
+       <Likes/>
+       <FooterForm/>
+    </>
+  )
+}
